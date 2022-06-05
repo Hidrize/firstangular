cb298c77a7e9451584e90d62c6eee532afa95676
Hidrize <Olanilekan@gmail.com>
2022-06-05 16:24:20 -0400

    fix the template error

--- firstangular/src/app/app.component.ts
+++ firstangular/src/app/app.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<ul> 
-  <li *ngif = "myArr; else idTemplate"
+  template: `<ul>
+  <li *ngif = "myArr; else idTemplate"> Type array is present </li>
+  </ul>
+  <ng-template  #idTemplate > Array is empty </ng-template> `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  
+
 myArr = ['a','e','i'];
 
 
